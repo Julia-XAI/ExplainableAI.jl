090a7fccc7ea8f376d54e462d40997ba0a25dc6b
Adrian Hill <adrian.hill@mailbox.org>
2021-02-17 19:03:44 +0100

    Add temporary tests of LRP functions

--- test/test_vgg19.jl
+++ test/test_vgg19.jl
@@ -4,17 +4,21 @@ using Metalhead
 # Load useful things from Metalhead.utils
 using Metalhead: classify, forward, labels, load_img
 using PrettyTables
+using LayerwiseRelevancePropagation
 
 vgg = VGG19()
 
 # ## Print layers in VGG19
 println("VGG-19 layers:")
-for layer in vgg.layers.layers
-    println("\t", layer)
+for (i, layer) in enumerate(vgg.layers.layers)
+    println("\t Layer $(i): ", layer)
+    # println("\t\t", size.(Flux.params(layer))) # access params
 end
 
+println("Param sizes: $(size.(Flux.params(vgg)))")
+
 # ## Load test image
-img = load("./test/img/elephant.jpg")
+img = load("./test/img/onion.jpg")
 
 # ## String output
 # This is equivalent to
@@ -23,12 +27,11 @@ img = load("./test/img/elephant.jpg")
 # ```
 classify(vgg, img)
 
-
 # Print sorted scores
 function print_scores(model, img; n=10)
     probs = forward(model, img) * 1f2 # class probabilities
     labs = labels(model)
-    ids = sortperm(scores;rev=true)[1:n] # indices of Top n classes
+    ids = sortperm(probs; rev=true)[1:n] # indices of Top n classes
 
     # Draw table
     data = hcat(labs[ids], probs[ids])
@@ -38,3 +41,27 @@ function print_scores(model, img; n=10)
 end
 
 print_scores(vgg, img)
+
+## Test LRP
+using Random
+using LayerwiseRelevancePropagation: _LRP
+rng = MersenneTwister(1234)
+
+dense_test = vgg.layers[27]
+W_test = Flux.params(dense_test)[1]
+isize = (4096,) # input dimension
+osize = Flux.outdims(dense_test, isize)
+
+a_in = randn(rng, Float32, isize)
+R_out = randn(rng, Float32, osize)
+
+println("Asdf:")
+println.(size.([a_in, W_test]))
+println("mult:", size(W_test * a_in))
+test = permutedims(a_in) * W_test'ᵀ
+println("mult:", size(test))
+
+R_in = LRP_generic(dense_test, a_in, R_out)
+
+
+Upper
