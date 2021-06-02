8f857be706f09b7bae808b3bd70b41d7e997eeac
Adrian Hill <adrian.hill@mailbox.org>
2021-06-02 22:09:46 +0200

    Update API
    default input to analyzers is now `input, output, class` to allow for analysis of different classes

--- src/api/explain.jl
+++ src/api/explain.jl
@@ -1,21 +1,58 @@
 abstract type AbstractXAIMethod end
 
-function explain(
-    input::AbstractArray,
-    analyzer::AbstractXAIMethod,
-    args...;
-    kwargs...
+"""
+Return raw classifier output and explanation.
+"""
+function output_and_explain(
+    input::AbstractArray{<:Real}, analyzer::AbstractXAIMethod, class::Integer, args...; kwargs...
 )
-    # Calling analyzers returns vals and explanations by default.
-    _, expl = analyzer(input)
+    output = analyzer.model(input)
+    expl = analyzer(input, output, class, args...; kwargs...)
+    return output, expl
+end
+
+"""
+Return raw classifier output and explanation for any output neuron `class`.
+"""
+function output_and_explain(
+    input::AbstractArray{<:Real}, analyzer::AbstractXAIMethod, args...; kwargs...
+)
+    output = analyzer.model(input)
+    expl = analyzer(input, output, argmax(output), args...; kwargs...)
+    return output, expl
+end
+
+"""
+Return explanation for neuron with highest activation.
+"""
+function explain(
+    input::AbstractArray{<:Real}, analyzer::AbstractXAIMethod, args...; kwargs...
+)
+    _, expl = output_and_explain(input, analyzer, args...; kwargs...)
     return expl
 end
 
+"""
+Return label of classification and explanation.
+"""
 function classify_and_explain(
-    input::AbstractArray,
+    input::AbstractArray{<:Real},
+    labels::AbstractVector{<:AbstractString},
     analyzer::AbstractXAIMethod,
     args...;
-    kwargs...
+    kwargs...,
 )
-    return analyzer(input)
+    output, expl = output_and_explain(input, analyzer, args...; kwargs...)
+    label = Flux.onecold(output, labels)
+    return label, expl
+end
+
+"""
+Return index of output neuron with highest activation and explanation.
+"""
+function classify_and_explain(
+    input::AbstractArray{<:Real}, analyzer::AbstractXAIMethod, args...; kwargs...
+)
+    output, expl = output_and_explain(input, analyzer, args...; kwargs...)
+    return argmax(output), expl
 end
