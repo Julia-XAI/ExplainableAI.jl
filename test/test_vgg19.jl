51891b80ae66cbb6355c0a1644cdd63e90970fae
Adrian Hill <adrian.hill@mailbox.org>
2021-06-02 22:07:57 +0200

    Update VGG19 test and assets

--- test/test_vgg19.jl
+++ test/test_vgg19.jl
@@ -1,67 +1,26 @@
 using ExplainabilityMethods
 using Flux
 using Metalhead
-using Metalhead: classify, forward, labels, load_img
 
-using ImageMagick # jpg I/O
-using PrettyTables
-
-vgg = VGG19()
-
-# ## Print layers in VGG19
-println("VGG-19 layers:")
-for (i, layer) in enumerate(vgg.layers.layers)
-    println("\t Layer $(i): ", layer)
-    # println("\t\t", size.(Flux.params(layer))) # access params
-end
-
-println("Param sizes: $(size.(Flux.params(vgg)))")
+using ImageCore
+using ImageMagick
+using ImageInTerminal
 
 # ## Load test image
-img = load("./test/img/onion.jpg")
+# And use `preprocess` to convert from CHW (Image.jl's channel ordering) to WHCN (Flux.jl's ordering)
+# and enforce Float32, as that seems important to Flux
+img = RGB.(load("./img/maltese.jpg"))
+imgp = Metalhead.preprocess(img)
 
-# ## String output
-# This is equivalent to
-# ```julia
-# classify(model::ClassificationModel, im) = Flux.onecold(forward(model, load_img(im)), labels(model))
-# ```
-classify(vgg, img)
+# ## Load VGG model
+vgg = VGG19()
+chain = vgg.layers
+labels = Metalhead.labels(vgg)
 
-# Print sorted scores
-function print_scores(model, img; n=10)
-    probs = forward(model, img) * 1f2 # class probabilities
-    labs = labels(model)
-    ids = sortperm(probs; rev=true)[1:n] # indices of Top n classes
+# model_summary(chain)
 
-    # Draw table
-    data = hcat(labs[ids], probs[ids])
-    header = ["Label" "Score [%]"]
-    pretty_table(data, header)
-    return nothing
-end
-
-print_scores(vgg, img)
-
-## Test LRP
-using Random
-using LayerwiseRelevancePropagation: _LRP
-rng = MersenneTwister(1234)
-
-dense_test = vgg.layers[27]
-W_test = Flux.params(dense_test)[1]
-isize = (4096,) # input dimension
-osize = Flux.outdims(dense_test, isize)
-
-a_in = randn(rng, Float32, isize)
-R_out = randn(rng, Float32, osize)
-
-println("Asdf:")
-println.(size.([a_in, W_test]))
-println("mult:", size(W_test * a_in))
-test = permutedims(a_in) * W_test'ᵀ
-println("mult:", size(test))
-
-R_in = LRP_generic(dense_test, a_in, R_out)
-
-
-Upper
+# ## Run analyzers
+analyzer = Gradient(chain)
+class, expl = classify_and_explain(imgp, labels, analyzer)
+println(class)
+imshow(Gray.(expl))
