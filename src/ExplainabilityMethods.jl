a582f1ce0a5d1fe114e6101d1d902d921b5fb95f
Adrian Hill <adrian.hill@mailbox.org>
2021-06-09 19:39:55 +0200

    Update deps

--- src/ExplainabilityMethods.jl
+++ src/ExplainabilityMethods.jl
@@ -3,10 +3,14 @@ module ExplainabilityMethods
 using LinearAlgebra
 using Flux
 using Statistics: std
+using PrettyTables: pretty_table
+using ColorSchemes
 
 include("api/explain.jl")
 include("methods/gradient.jl")
 include("methods/lrp/rules.jl")
+include("heatmap.jl")
+include("utils.jl")
 
 export explain, classify_and_explain
 
@@ -16,4 +20,6 @@ export Gradient
 # rules
 export LRP_0, LRP_γ, LRP_ϵ, LRP_zᴮ
 
+# heatmapping
+export heatmap
 end # module
