c1bdce0f66cd38d7874b8f188f5850bc7beae14e
Adrian Hill <adrian.hill@mailbox.org>
2021-10-11 21:18:37 +0200

    Refactor rules and rules-matching

--- src/methods/lrp/rules.jl
+++ src/methods/lrp/rules.jl
@@ -1,52 +1,91 @@
-abstract type AbstractReverseRule end
+
+# Generic implementation of LRP according to [1, 2].
+# LRP-rules are implemented as structs of type `AbstractLRPRule`.
+# Through the magic of multiple dispatch, rule modifications such as LRP-γ and -ϵ
+# can be implemented by dispatching on the functions `modify_params` & `modify_denominator`,
+# which make use of the generalized LRP implementation shown in [1].
+
+# If the relevance propagation falls outside of this scheme, a custom function
+# ```julia
+# (::MyLRPRule)(layer, aₖ, Rₖ₊₁) = ...
+# ```
+# can be implemented. This is used for the ZBoxRule.
+#
+# References:
+# [1] G. Montavon et al., Layer-Wise Relevance Propagation: An Overview
+# [2] W. Samek et al., Explaining Deep Neural Networks and Beyond:
+#     A Review of Methods and Applications
+
+abstract type AbstractLRPRule end
 
 """
-Common layer types LRP rules work on.
+    ZeroRule()
+
+Constructor for LRP-0 rule. Commonly used on upper layers.
 """
-CommonLayer = Union{Dense,Conv,MaxPool}
+struct ZeroRule <: AbstractLRPRule end
 
 """
-Constructs generic LRP rules, implemented according to [1, 2].
+    GammaRule(; γ=0.25)
+
+Constructor for LRP-``γ`` rule. Commonly used on lower layers.
 
 Arguments:
-- `layer`: A Flux layer.
-- `rulename`: The name of the rule used for printing the LRPChain summary
-- `ρ`: Function that modifies weights and biases as proposed in [1].
-- `add_ϵ`: Function that increments zₖ on the forward pass for stability [1].
-
-Default kwargs correspond to the basic LRP-0 rule.
-
-References:
-[1] G. Montavon et al., Layer-Wise Relevance Propagation: An Overview
-[2] W. Samek et al., Explaining Deep Neural Networks and Beyond:
-    A Review of Methods and Applications
+- `γ`: Optional multiplier for added positive weights, defaults to 0.25.
 """
-struct LRPRule{T,L,W,B} <: AbstractReverseRule
-    layer::L # Flux layer
-    ρW::W # weights after applying modifier ρ
-    b::B # biases after applying modifier ρ
-    add_ϵ::Function # increments zₖ on forward pass
-    rulename::String
-
-    function LRPRule(layer, rulename; ρ=identity, add_ϵ=identity, T=Float32)
-        ρW = T.(ρ(layer.weight))
-
-        if typeof(layer.bias) <: Flux.Zeros
-            b = zeros(T, size(ρW, 1))
-        else
-            b = T.(layer.bias)
-        end
-        return new{T, typeof(layer),typeof(ρW),typeof(b)}(layer, ρW, b, add_ϵ, rulename)
-    end
+struct GammaRule{T} <: AbstractLRPRule
+    γ::T
+    GammaRule(; γ=0.25) = new{Float32}(γ)
 end
 
-function (r::LRPRule{T,L,W,B})(
-    aₖ::AbstractArray, # activations (forward)
-    Rₖ₊₁::AbstractArray, # relevance scores (backward)
-) where {T,L<:CommonLayer,W,B}
+"""
+    EpsilonRule(; ε=1f-6)
+
+Constructor for LRP-``ϵ`` rule. Commonly used on middle layers.
+
+Arguments:
+- `ϵ`: Optional stabilization parameter, defaults to `1f-6`.
+"""
+struct EpsilonRule{T} <: AbstractLRPRule
+    ε::T
+    EpsilonRule(; ε=1f-6) = new{Float32}(ε)
+end
+
+"""
+    ZBoxRule()
+
+Constructor for LRP-`z^{\\mathcal{B}}``-rule.
+Commonly used on the first layer for pixel input.
+"""
+struct ZBoxRule <: AbstractLRPRule end
+
+"""
+    modify_params!(w, rule)
+
+Function that modifies weights and biases before applying relevance propagation.
+"""
+modify_params(w, ::AbstractReverseRule) = w
+modify_params(w, r::GammaRule) = w + r.γ * relu.(w)
+
+"""
+    modify_denominator!(d, rule)
+
+Function that increments zₖ on the forward pass for numerical stability.
+"""
+modify_denominator(d, ::AbstractReverseRule) = d
+modify_denominator(d, r::EpsilonRule) = (1 + r.ϵ * std(d)) * d
+
+LinearLayer = Union{Dense,Conv,MaxPool}
+
+function (rule::AbstractLRPRule)(
+    layer::L, aₖ::AbstractArray, Rₖ₊₁::AbstractArray
+) where {L<:LinearLayer}
     # Forward pass
+    W, b = get_weights(layer)
+    ρW = modify_params(W, rule)
+
     function fwpass(a)
-        z = r.add_ϵ(r.ρW * a + r.b)
+        z = modify_denominator(ρW * a + b, rule)
         s = Rₖ₊₁ ./ (z .+ 1e-9)
         return z ⋅ s
     end
@@ -57,87 +96,25 @@ function (r::LRPRule{T,L,W,B})(
     return Rₖ
 end
 
-"""
-Constructor for LRP-0 rule. Commonly used on upper layers.
-"""
-LRP_0(l; kwargs...) = LRPRule(l, "LRP-0"; kwargs...)
+# The ZBoxRule requires its own implementation of relevance propagation.
+function (::ZBoxRule)(
+    layer::L, aₖ::AbstractArray, Rₖ₊₁::AbstractArray
+) where {L<:LinearLayer}
+    W, b = get_weights(layer)
+    W⁻ = min.(0, W)
+    W⁺ = max.(0, W)
+    b⁻ = min.(0, b)
+    b⁺ = max.(0, b)
 
-"""
-Constructor for LRP-``ϵ`` rule. Commonly used on middle layers.
-
-Arguments:
-- `ϵ`: Optional stabilization parameter, defaults to `1f-6`.
-"""
-function LRP_ϵ(l; ϵ=1f-6, kwargs...)
-    _add_ϵ(z) = (1 + ϵ * std(z)) * z
-    return LRPRule(l, "LRP-ϵ, ϵ=$(ϵ)"; add_ϵ=_add_ϵ, kwargs...)
-end
-
-"""
-Constructor for LRP-``γ`` rule. Commonly used on lower layers.
-
-Arguments:
-- `γ`: Optional multiplier for added positive weights, defaults to 0.25.
-"""
-function LRP_γ(l; γ=0.25, kwargs...)
-    _ρ(w) = w + γ * relu.(w)
-    return LRPRule(l, "LRP-γ, γ=$(γ)"; ρ=_ρ, kwargs...)
-end
-
-"""
-Implements generic ``z^{\\mathcal{B}}``-rule., implemented according to [1, 2].
-
-Arguments:
-- `layer`: A Flux layer.
-- `rulename`: The name of the rule used for printing the LRPChain summary
-- `ρ`: Function that modifies weights and biases as proposed in [1].
-
-Default kwargs correspond to the basic LRP-0 rule.
-
-References:
-[1] G. Montavon et al., Layer-Wise Relevance Propagation: An Overview
-[2] W. Samek et al., Explaining Deep Neural Networks and Beyond:
-    A Review of Methods and Applications
-"""
-struct LRPZBoxRule{T,L,W,B} <: AbstractReverseRule
-    layer::L # Flux layer
-    W::W # weights
-    W⁻::W
-    W⁺::W
-    b::B # biases
-    b⁻::B
-    b⁺::B
-    rulename::String
-
-    function LRPZBoxRule(layer, rulename; ρ=identity, T=Float32)
-        W = T.(ρ(layer.weight))
-        W⁻ = min.(0, W)
-        W⁺ = max.(0, W)
-
-        if typeof(layer.bias) <: Flux.Zeros
-            b = zeros(T, size(ρW, 1))
-        else
-            b = T.(layer.bias)
-        end
-        b⁻ = min.(0, b)
-        b⁺ = max.(0, b)
-
-        return new{T, typeof(layer),typeof(W),typeof(b)}(layer, W, W⁻, W⁺, b, b⁻, b⁺, rulename)
-    end
-end
-
-function (r::LRPZBoxRule{T,L,W,B})(
-    aₖ::AbstractArray, Rₖ₊₁::AbstractArray
-) where {T,L<:CommonLayer,W,B}
     onemat = ones(T, size(aₖ))
     l = onemat * minimum(aₖ)
     h = onemat * maximum(aₖ)
 
     # Forward pass
     function fwpass(a)
-        f = r.W * a + r.b
-        f⁺ = r.W⁺ * l + r.b⁺
-        f⁻ = r.W⁻ * l + r.b⁻
+        f = W * a + b
+        f⁺ = W⁺ * l + b⁺
+        f⁻ = W⁻ * h + b⁻
 
         z = f - f⁺ - f⁻
         s = Rₖ₊₁ ./ (z .+ 1e-9)
@@ -157,8 +134,13 @@ function (r::LRPZBoxRule{T,L,W,B})(
     return Rₖ
 end
 
-"""
-Constructor for LRP-`z^{\\mathcal{B}}``-rule.
-Commonly used on the first layer for pixel input.
-"""
-LRP_zᴮ(l; kwargs...) = LRPZBoxRule(l, "LRP-zᴮ"; kwargs...)
+# useful helper function to work around Flux.Zeros
+function get_weights(layer)
+    W = layer.weight
+    if typeof(layer.bias) <: Flux.Zeros
+        b = zeros(eltype(W), size(W, 1))
+    else
+        b = layer.bias
+    end
+    return W, b
+end
