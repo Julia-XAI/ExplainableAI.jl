5565ca22bd7973db5a4c783c7dffa0829dd2469a
Adrian Hill <adrian.hill@mailbox.org>
2021-10-19 18:12:14 +0200

    Test rules in Float32

--- test/test_rules.jl
+++ test/test_rules.jl
@@ -6,7 +6,7 @@ using ReferenceTests
 using Random
 
 # Fixed pseudo-random numbers
-T = Float64
+T = Float32
 pseudorandn(dims...) = randn(MersenneTwister(123), T, dims...)
 
 # Define test layers
@@ -20,13 +20,6 @@ layers = Dict(
 
 # Define Dense test input
 aₖ = pseudorandn(ins)
-
-function approxref(ref::String, act::String)
-    # Parse reference string into array and compare
-    ref = eval(Meta.parse(ref))
-    act = eval(Meta.parse(act))
-    return ref ≈ act
-end
 
 for (rulename, ruletype) in RULES
     rule = ruletype()
@@ -47,7 +40,7 @@ for (rulename, ruletype) in RULES
                     @test all(Rₖ[outs:end] .< 1e-8)
                 end
 
-                @test_reference "references/rules/$(rulename)_$(layername).txt" Rₖ by=approxref
+                @test_reference "references/rules/$(rulename)_$(layername).txt" Rₖ
             end
         end
     end
