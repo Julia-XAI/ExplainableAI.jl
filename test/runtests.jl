0f108f9df0b662f056b012b942cdc937c735e7e2
Adrian Hill <adrian.hill@mailbox.org>
2021-10-27 14:50:40 +0200

    Rename tests on Flux utils

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using ExplainabilityMethods
 using Test
 
 @testset "ExplainabilityMethods.jl" begin
-    @testset "Utilities" begin
-        include("test_utils.jl")
+    @testset "Flux utilities" begin
+        include("test_flux_utils.jl")
     end
     @testset "LRP rules" begin
         include("test_rules.jl")
