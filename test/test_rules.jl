696920ffa7b02f9b6e00e22db064028a929a289c
Adrian Hill <adrian.hill@mailbox.org>
2021-10-27 14:53:20 +0200

    Add tests on conv- and pooling-layers

--- test/test_rules.jl
+++ test/test_rules.jl
@@ -55,12 +55,68 @@ layers = Dict(
         end
     end
 end
+
+## Test PoolingLayers
+insize = (6, 6, 2, 1)
+aₖ = pseudorandn(insize)
+
+equalpairs = Dict( # these pairs of layers are all equal
+    "AdaptiveMaxPool" => (AdaptiveMaxPool((3, 3)), MaxPool((2, 2); pad=0)),
+    "GlobalMaxPool" => (GlobalMaxPool(), MaxPool((6, 6); pad=0)),
+    "AdaptiveMeanPool" => (AdaptiveMeanPool((3, 3)), MeanPool((2, 2); pad=0)),
+    "GlobalMeanPool" => (GlobalMeanPool(), MeanPool((6, 6); pad=0)),
+)
+
+@testset "PoolingLayers" begin
+    for (rulename, ruletype) in RULES
+        rule = ruletype()
+        @testset "$rulename" begin
+            for (layername, layers) in equalpairs
+                @testset "$layername" begin
+                    l1, l2 = layers
+                    Rₖ₊₁ = l1(aₖ)
+                    Rₖ₊₁ == l2(aₖ)
+                    Rₖ = rule(l1, aₖ, Rₖ₊₁)
+                    Rₖ == rule(l2, aₖ, Rₖ₊₁)
+
+                    @test typeof(Rₖ) == typeof(aₖ)
+                    @test size(Rₖ) == size(aₖ)
+
+                    @test_reference "references/rules/$rulename/$layername.jld2" Dict(
+                        "R" => Rₖ
+                    ) by = (r, a) -> isapprox(r["R"], a["R"]; rtol=0.02)
+                end
+            end
+        end
+    end
 end
 
-# Test individual rules
-@testset "modify_params" begin
-    W, b = [1.0 -1.0; 2.0 0.0], [-1.0, 1.0]
-    ρW, ρb = modify_params(GammaRule(; γ=0.42), W, b)
-    @test ρW ≈ [1.42 -1.0; 2.84 0.0]
-    @test ρb ≈ [-1.0, 1.42]
+## Test ConvLayers and others
+layers = Dict(
+    "Conv" => Conv((3, 3), 2 => 4; init=pseudorandn),
+    "DepthwiseConv" => DepthwiseConv((3, 3), 2 => 4; init=pseudorandn),
+    "ConvTranspose" => ConvTranspose((3, 3), 2 => 4; init=pseudorandn),
+    "CrossCor" => CrossCor((3, 3), 2 => 4; init=pseudorandn),
+    "flatten" => flatten,
+    "Dropout" => Dropout(0.2),
+)
+@testset "Other Layers" begin
+    for (rulename, ruletype) in RULES
+        rule = ruletype()
+        @testset "$rulename" begin
+            for (layername, layer) in layers
+                @testset "$layername" begin
+                    Rₖ₊₁ = layer(aₖ)
+                    Rₖ = rule(layer, aₖ, Rₖ₊₁)
+
+                    @test typeof(Rₖ) == typeof(aₖ)
+                    @test size(Rₖ) == size(aₖ)
+
+                    @test_reference "references/rules/$rulename/$layername.jld2" Dict(
+                        "R" => Rₖ
+                    ) by = (r, a) -> isapprox(r["R"], a["R"]; rtol=0.02)
+                end
+            end
+        end
+    end
 end
