614bdb7bf9e1bf62b5cd94521bc805a518d7fd02
Adrian Hill <adrian.hill@mailbox.org>
2022-03-01 00:35:40 +0100

    Restructure tests (#24)

--- test/test_vgg19.jl
+++ test/test_vgg19.jl
@@ -1,11 +1,18 @@
 using ExplainabilityMethods
-using ExplainabilityMethods: ANALYZERS
 using Flux
 using JLD2
 
+const GRADIENT_ANALYZERS = Dict(
+    "Gradient" => Gradient, "InputTimesGradient" => InputTimesGradient
+)
+const LRP_ANALYZERS = Dict(
+    "LRPZero" => LRPZero, "LRPEpsilon" => LRPEpsilon, "LRPGamma" => LRPGamma
+)
+
 using Random
 pseudorand(T, dims...) = rand(MersenneTwister(123), T, dims...)
-img = pseudorand(Float32, (224, 224, 3, 1))
+input_size = (224, 224, 3, 1)
+img = pseudorand(Float32, input_size)
 
 # Load VGG model:
 # We run the reference test on the randomly intialized weights
@@ -14,25 +21,37 @@ include("./vgg19.jl")
 vgg19 = VGG19(; pretrain=false)
 model = flatten_chain(strip_softmax(vgg19.layers))
 
-# Run analyzers
-analyzers = ANALYZERS
 function LRPCustom(model::Chain)
     return LRP(model, [ZBoxRule(), repeat([GammaRule()], length(model.layers) - 1)...])
 end
-analyzers["LRPCustom"] = LRPCustom
 
-for (name, method) in analyzers
+println("Running tests on VGG16...")
+
+function test_vgg16(name, method)
     @time @testset "$name" begin
-        print("Timing $name on VGG19...")
-        if name == "LRP"
-            analyzer = method(model, ZeroRule())
-        else
-            analyzer = method(model)
-        end
+        print("Timing $name...\t")
+        analyzer = method(model)
         expl, _ = analyze(img, analyzer)
 
         @test size(expl) == size(img)
         @test_reference "references/vgg19/$(name).jld2" Dict("expl" => expl) by =
             (r, a) -> isapprox(r["expl"], a["expl"]; rtol=0.05)
+    end
+    return nothing
+end
+
+# Run analyzers
+@testset "LRP analyzers" begin
+    for (name, method) in LRP_ANALYZERS
+        test_vgg16(name, method)
+    end
+end
+@testset "Custom LRP composite" begin
+    test_vgg16("LRPCustom", LRPCustom)
+end
+
+@testset "Gradient analyzers" begin
+    for (name, method) in GRADIENT_ANALYZERS
+        test_vgg16(name, method)
     end
 end
