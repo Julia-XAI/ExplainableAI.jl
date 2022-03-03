7e6d501d4220402d2325ecb77aa85f7450c09c6f
Adrian Hill <adrian.hill@mailbox.org>
2022-03-03 18:17:43 +0100

    Extend benchmarks to rules and AD fallback

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,6 +1,7 @@
-using ExplainabilityMethods
-using ExplainabilityMethods: ANALYZERS
+using BenchmarkTools
 using Flux
+using Metalhead
+using ExplainabilityMethods
 
 on_CI = haskey(ENV, "GITHUB_ACTIONS")
 
@@ -24,10 +25,49 @@ algs = Dict(
 
 # Define benchmark
 SUITE = BenchmarkGroup()
+SUITE["VGG"] = BenchmarkGroup([k for k in keys(algs)])
 for (name, alg) in algs
-    SUITE[name] = BenchmarkGroup(["construct analyzer", "analyze"])
-    SUITE[name]["construct analyzer"] = @benchmarkable alg($(model))
+    SUITE["VGG"][name] = BenchmarkGroup(["construct analyzer", "analyze"])
+    SUITE["VGG"][name]["construct analyzer"] = @benchmarkable alg($(model))
 
     analyzer = alg(model)
-    SUITE[name]["analyze"] = @benchmarkable analyze($(img), $(analyzer))
+    SUITE["VGG"][name]["analyze"] = @benchmarkable analyze($(img), $(analyzer))
+end
+
+# Rules benchmarks – use wrapper to trigger AD fallback
+struct TestWrapper{T}
+    layer::T
+end
+(l::TestWrapper)(x) = l.layer(x)
+
+# generate input for conv layers
+insize = (128, 128, 3, 1)
+aₖ = randn(Float32, insize)
+
+layers = Dict(
+    "MaxPool" => (MaxPool((3, 3); pad=0), aₖ),
+    "MeanPool" => (MeanPool((3, 3); pad=0), aₖ),
+    "Conv" => (Conv((3, 3), 3 => 6), aₖ),
+    "flatten" => (flatten, aₖ),
+    "Dense" => (Dense(1000, 200, relu), randn(Float32, 1000)),
+)
+rules = Dict(
+    "ZeroRule" => ZeroRule(),
+    "EpsilonRule" => EpsilonRule(),
+    "GammaRule" => GammaRule(),
+    "ZBoxRule" => ZBoxRule(),
+)
+rulenames = [k for k in keys(rules)]
+
+for (layername, (layer, aₖ)) in layers
+    SUITE[layername] = BenchmarkGroup(rulenames)
+
+    for (rulename, ruletype) in rules
+        Rₖ₊₁ = layer(aₖ)
+        SUITE[layername][rulename] = BenchmarkGroup(["dispatch", "AD fallback"])
+        SUITE[layername][rulename]["dispatch"] = @benchmarkable rule($layer, $aₖ, $Rₖ₊₁)
+        SUITE[layername][rulename]["AD fallback"] = @benchmarkable rule(
+            $TestWrapper(layer), $aₖ, $Rₖ₊₁
+        )
+    end
 end
