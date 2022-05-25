4d07c86114181c83c2627e72206f01ce13d6ef14
Adrian Hill <adrian.hill@mailbox.org>
2022-05-25 18:24:37 +0200

    Add back tests on gradient methods

--- test/test_vgg11.jl
+++ test/test_vgg11.jl
@@ -51,9 +51,7 @@ function test_vgg11(name, method; kwargs...)
         analyzer = method(model)
         h2 = heatmap(img, analyzer; kwargs...)
         @test h1 ≈ h2
-        if expl.analyzer != :Gradient # TODO: remove
-            @test_reference "references/heatmaps/vgg11_$(name).txt" h1
-        end
+        @test_reference "references/heatmaps/vgg11_$(name).txt" h1
     end
     @testset "$name neuron selection" begin
         analyzer = method(model)
