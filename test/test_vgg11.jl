3204e93b3529dc94a9b2f8321b39b28956a9166f
Adrian Hill <adrian.hill@mailbox.org>
2022-07-01 17:34:34 +0200

    Update tests

--- test/test_vgg11.jl
+++ test/test_vgg11.jl
@@ -27,25 +27,25 @@ function LRPCustom(model::Chain)
     return LRP(
         model,
         [
-            ZBoxRule(0.0f0, 1.0f0),
-            EpsilonRule(),
-            GammaRule(),
-            EpsilonRule(),
-            GammaRule(),
-            GammaRule(),
-            EpsilonRule(),
-            GammaRule(),
-            GammaRule(),
-            EpsilonRule(),
-            GammaRule(),
-            GammaRule(),
-            EpsilonRule(),
-            ZeroRule(),
-            ZeroRule(),
-            ZeroRule(),
-            ZeroRule(),
-            ZeroRule(),
-            ZeroRule(),
+            ZBoxRule(0.0f0, 1.0f0), # Conv((3, 3), 3 => 64, relu, pad=1),   # 1_792 parameters
+            EpsilonRule(), # MaxPool((2, 2)),
+            GammaRule(), # Conv((3, 3), 64 => 128, relu, pad=1),  # 73_856 parameters
+            EpsilonRule(), # MaxPool((2, 2)),
+            GammaRule(), # Conv((3, 3), 128 => 256, relu, pad=1),  # 295_168 parameters
+            GammaRule(), # Conv((3, 3), 256 => 256, relu, pad=1),  # 590_080 parameters
+            EpsilonRule(), # MaxPool((2, 2)),
+            GammaRule(), # Conv((3, 3), 256 => 512, relu, pad=1),  # 1_180_160 parameters
+            GammaRule(), # Conv((3, 3), 512 => 512, relu, pad=1),  # 2_359_808 parameters
+            EpsilonRule(), # MaxPool((2, 2)),
+            GammaRule(), # Conv((3, 3), 512 => 512, relu, pad=1),  # 2_359_808 parameters
+            GammaRule(), # Conv((3, 3), 512 => 512, relu, pad=1),  # 2_359_808 parameters
+            EpsilonRule(), # MaxPool((2, 2)),
+            PassRule(), # Flux.flatten,
+            ZeroRule(), # Dense(25088 => 4096, relu),           # 102_764_544 parameters
+            PassRule(), # Dropout(0.5),
+            ZeroRule(), # Dense(4096 => 4096, relu),            # 16_781_312 parameters
+            PassRule(), # Dropout(0.5),
+            ZeroRule(), # Dense(4096 => 1000),                  # 4_097_000 parameters
         ],
     )
 end
