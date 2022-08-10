7180afe372e47c1a0001cd78f837d84204d5f8be
Documenter.jl <documenter@juliadocs.github.io>
2022-08-10 09:19:55 +0000

    build based on ac52d64

--- dev/generated/advanced_lrp.jl
+++ dev/generated/advanced_lrp.jl
@@ -26,17 +26,19 @@ heatmap(input, analyzer)
 
 composite = Composite(
     ZeroRule(),                         # default rule
-    GlobalRuleMap(
+    GlobalTypeRule(
         Conv => GammaRule(),       # apply GammaRule on all convolutional layers
         MaxPool => EpsilonRule(),  # apply EpsilonRule on all pooling-layers
     ),
-    FirstRule(ZBoxRule(0.0f0, 1.0f0)),  # apply ZBoxRule on the first layer
+    FirstLayerRule(ZBoxRule(0.0f0, 1.0f0)),  # apply ZBoxRule on the first layer
 )
 
 analyzer = LRP(model, composite)        # construct LRP analyzer from composite
 heatmap(input, analyzer)
 
 analyzer.rules # show rules
+
+EpsilonPlusFlat()
 
 struct MyGammaRule <: AbstractLRPRule end
 
