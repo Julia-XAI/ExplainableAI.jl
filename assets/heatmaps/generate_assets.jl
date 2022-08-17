6c54f67bb691328c2cb8fb1086d310cd34eb59af
Adrian Hill <adrian.hill@mailbox.org>
2022-08-17 16:31:22 +0200

    Update heatmap assets

--- assets/heatmaps/generate_assets.jl
+++ assets/heatmaps/generate_assets.jl
@@ -8,7 +8,8 @@ model = VGG(16; pretrain=true).layers
 model = strip_softmax(flatten_chain(model))
 
 # Load input
-img = load("./assets/heatmaps/castle.jpg")
+url = "https://raw.githubusercontent.com/adrhill/ExplainableAI.jl/gh-pages/assets/heatmaps/castle.jpg"
+img = load(url)
 input = preprocess_imagenet(img)
 input = reshape(input, 224, 224, 3, :)  # reshape to WHCN format
 
@@ -19,15 +20,18 @@ methods = Dict(
     "SmoothGrad" => SmoothGrad,
     "IntegratedGradients" => IntegratedGradients,
     "LRP" => LRP,
-    "LRPEpsilonGammaBox" => m -> LRP(m, EpsilonGammaBox(-3.0f0, 3.0f0)),
-    # "LRPEpsilonPlus" => m -> LRP(m, EpsilonPlus()),
-    # "LRPEpsilonAlpha2Beta1" => m -> LRP(m, EpsilonAlpha2Beta1()),
-    # "LRPEpsilonPlusFlat" => m -> LRP(m, EpsilonPlusFlat()),
-    # "LRPEpsilonAlpha2Beta1Flat" => m -> LRP(m, EpsilonAlpha2Beta1Flat()),
+    "LRPEpsilonGammaBox" => model -> LRP(model, EpsilonGammaBox(-3.0f0, 3.0f0)),
+    "LRPEpsilonPlus" => model -> LRP(model, EpsilonPlus()),
+    "LRPEpsilonAlpha2Beta1" => model -> LRP(model, EpsilonAlpha2Beta1()),
+    "LRPEpsilonPlusFlat" => model -> LRP(model, EpsilonPlusFlat()),
+    "LRPEpsilonAlpha2Beta1Flat" => model -> LRP(model, EpsilonAlpha2Beta1Flat()),
 )
 
 for (name, method) in methods
+    @info "Generating $name assets..."
     analyzer = method(model)
+
+    # Max activated neuron corresponds to "castle"
     h = heatmap(input, analyzer)
     save("castle_$name.png", h)
 
