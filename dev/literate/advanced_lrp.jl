23130e34d0fd5aa323dbc05a72d76525c6b1d32d
Documenter.jl <documenter@juliadocs.github.io>
2022-11-11 14:32:55 +0000

    build based on ed9436f

---
+++ dev/literate/advanced_lrp.jl
@@ -0,0 +1,356 @@
+# # Advanced LRP usage
+# One of the design goals of ExplainableAI.jl is to combine ease of use and
+# extensibility for the purpose of research.
+#
+#
+# This example will show you how to implement custom LRP rules and register custom layers
+# and activation functions.
+# For this purpose, we will quickly load our model from the previous section
+using ExplainableAI
+using Flux
+using MLDatasets
+using ImageCore
+using BSON
+
+model = BSON.load("../model.bson", @__MODULE__)[:model]
+
+# and data from the MNIST dataset
+index = 10
+x, _ = MNIST(Float32, :test)[10]
+input = reshape(x, 28, 28, 1, :);
+
+# ## LRP composites
+# ### Assigning individual rules
+# When creating an LRP-analyzer, we can assign individual rules to each layer.
+# The array of rules has to match the length of the Flux chain.
+# The `LRP` analyzer will show a summary of how layers and rules got matched:
+rules = [
+    ZBoxRule(0.0f0, 1.0f0),
+    EpsilonRule(),
+    GammaRule(),
+    EpsilonRule(),
+    ZeroRule(),
+    ZeroRule(),
+    ZeroRule(),
+    ZeroRule(),
+]
+
+analyzer = LRP(model, rules)
+
+#
+heatmap(input, analyzer)
+
+# Since some Flux Chains contain other Flux Chains, ExplainableAI provides
+# a utility function called [`flatten_model`](@ref).
+#
+#md # !!! warning "Flattening models"
+#md #     Not all models can be flattened, e.g. those using
+#md #     `Parallel` and `SkipConnection` layers.
+
+# ### Custom composites
+# Instead of manually defining a list of rules, we can also use a [`Composite`](@ref).
+# A composite contructs a list of LRP-rules by sequentially applying
+# [Composite primitives](@ref composite_primitive_api) it contains.
+#
+# To obtain the same set of rules as in the previous example, we can define
+composite = Composite(
+    ZeroRule(),                              # default rule
+    GlobalTypeRule(
+        Conv => GammaRule(),                 # apply GammaRule on all convolutional layers
+        MaxPool => EpsilonRule(),            # apply EpsilonRule on all pooling-layers
+    ),
+    FirstLayerRule(ZBoxRule(0.0f0, 1.0f0)),  # apply ZBoxRule on the first layer
+)
+
+# We now construct an LRP analyzer from `composite`
+analyzer = LRP(model, composite)
+
+# As you can see, this analyzer contains the same rules as our previous one
+# and therefore also produces the same heatmaps:
+heatmap(input, analyzer)
+
+# ### Composite primitives
+# The following [Composite primitives](@ref composite_primitive_api) can used to construct a [`Composite`](@ref).
+#
+# To apply a single rule, use:
+# * [`LayerRule`](@ref) to apply a rule to the `n`-th layer of a model
+# * [`GlobalRule`](@ref) to apply a rule to all layers
+# * [`RangeRule`](@ref) to apply a rule to a positional range of layers
+# * [`FirstLayerRule`](@ref) to apply a rule to the first layer
+# * [`LastLayerRule`](@ref) to apply a rule to the last layer
+#
+# To apply a set of rules to layers based on their type, use:
+# * [`GlobalTypeRule`](@ref) to apply a dictionary that maps layer types to LRP-rules
+# * [`RangeTypeRule`](@ref) for a `TypeRule` on generalized ranges
+# * [`FirstLayerTypeRule`](@ref) for a `TypeRule` on the first layer of a model
+# * [`LastLayerTypeRule`](@ref) for a `TypeRule` on the last layer
+# * [`FirstNTypeRule`](@ref) for a `TypeRule` on the first `n` layers
+# * [`LastNTypeRule`](@ref) for a `TypeRule` on the last `n` layers
+#
+# Primitives are called sequentially in the order the `Composite` was created with
+# and overwrite rules specified by previous primitives.
+
+# ### Default composites
+# A list of implemented default composites can be found under
+# [Default composites](@ref default_composite_api) in the API reference, e.g. [`EpsilonPlusFlat`](@ref):
+composite = EpsilonPlusFlat()
+#
+analyzer = LRP(model, composite)
+
+# ## Custom LRP rules
+# Let's define a rule that modifies the weights and biases of our layer on the forward pass.
+# The rule has to be of type `AbstractLRPRule`.
+struct MyGammaRule <: AbstractLRPRule end
+
+# It is then possible to dispatch on the utility functions [`modify_input`](@ref),
+# [`modify_parameters`](@ref) and [`modify_denominator`](@ref) with the rule type
+# `MyCustomLRPRule` to define custom rules without writing any boilerplate code.
+# To extend internal functions, import them explicitly:
+import ExplainableAI: modify_parameters
+
+modify_parameters(::MyGammaRule, param) = param + 0.25f0 * relu.(param)
+
+# We can directly use this rule to make an analyzer!
+rules = [
+    ZBoxRule(0.0f0, 1.0f0),
+    EpsilonRule(),
+    MyGammaRule(),
+    EpsilonRule(),
+    ZeroRule(),
+    ZeroRule(),
+    ZeroRule(),
+    ZeroRule(),
+]
+analyzer = LRP(model, rules)
+heatmap(input, analyzer)
+
+# We just implemented our own version of the ``γ``-rule in 2 lines of code.
+# The heatmap perfectly matches the previous one!
+
+# For more granular control over weights and biases, [`modify_weight`](@ref)
+# and [`modify_bias`](@ref) can be used.
+# If the layer doesn't use weights `layer.weight` and biases `layer.bias`,
+# ExplainableAI provides a lower-level variant of [`modify_parameters`](@ref)
+# called [`modify_layer`](@ref). This function is expected to take a layer
+# and return a new, modified layer.
+# To add compatibility checks between rule and layer types, extend [`is_compatible`](@ref).
+
+#md # !!! warning "Extending modify_layer"
+#md #
+#md #     Use of the function `modify_layer` will overwrite functionality of
+#md #     `modify_parameters`, `modify_weight` and `modify_bias`
+#md #     for the implemented combination of rule and layer types.
+#md #     This is due to the fact that internally, `modify_weight` and `modify_bias`
+#md #     are called by the default implementation of `modify_layer`.
+#md #     `modify_weight` and `modify_bias` in turn call `modify_parameters` by default.
+#md #
+#md #     Therefore `modify_layer` should only be extended for a specific rule
+#md #     and a specific layer type.
+
+# ## Custom layers and activation functions
+# ### Model checks for humans
+# Good model checks and presets should allow novice users to apply XAI methods
+# in a "plug & play" manner according to best practices.
+#
+# Let's say we define a layer that doubles its input:
+struct MyDoublingLayer end
+(::MyDoublingLayer)(x) = 2 * x
+
+mylayer = MyDoublingLayer()
+mylayer([1, 2, 3])
+
+# Let's append this layer to our model:
+model = Chain(model..., MyDoublingLayer())
+
+# Creating an LRP analyzer, e.g. `LRP(model)`, will throw an `ArgumentError`
+# and print a summary of the model check in the REPL:
+# ```julia-repl
+# ┌───┬───────────────────────┬─────────────────┬────────────┬────────────────┐
+# │   │ Layer                 │ Layer supported │ Activation │ Act. supported │
+# ├───┼───────────────────────┼─────────────────┼────────────┼────────────────┤
+# │ 1 │ flatten               │            true │     —      │           true │
+# │ 2 │ Dense(784, 100, relu) │            true │    relu    │           true │
+# │ 3 │ Dense(100, 10)        │            true │  identity  │           true │
+# │ 4 │ MyDoublingLayer()     │           false │     —      │           true │
+# └───┴───────────────────────┴─────────────────┴────────────┴────────────────┘
+#   Layers failed model check
+#   ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+#
+#   Found unknown layers MyDoublingLayer() that are not supported by ExplainableAI's LRP implementation yet.
+#
+#   If you think the missing layer should be supported by default, please submit an issue (https://github.com/adrhill/ExplainableAI.jl/issues).
+#
+#   These model checks can be skipped at your own risk by setting the LRP-analyzer keyword argument skip_checks=true.
+#
+#   [...]
+# ```
+
+# LRP should only be used on deep rectifier networks and ExplainableAI doesn't
+# recognize `MyDoublingLayer` as a compatible layer.
+# By default, it will therefore return an error and a model check summary
+# instead of returning an incorrect explanation.
+#
+# However, if we know `MyDoublingLayer` is compatible with deep rectifier networks,
+# we can register it to tell ExplainableAI that it is ok to use.
+# This will be shown in the following section.
+
+#md # !!! warning "Skipping model checks"
+#md #
+#md #     All model checks can be skipped at the user's own risk by setting the LRP-analyzer
+#md #     keyword argument `skip_checks=true`.
+
+# ### Registering custom layers
+# The error in the model check will stop after registering our custom layer type
+# `MyDoublingLayer` as "supported" by ExplainableAI.
+#
+# This is done using the function [`LRP_CONFIG.supports_layer`](@ref),
+# which should be set to return `true` for the type `MyDoublingLayer`:
+LRP_CONFIG.supports_layer(::MyDoublingLayer) = true
+
+# Now we can create and run an analyzer without getting an error:
+analyzer = LRP(model)
+heatmap(input, analyzer)
+
+#md # !!! note "Registering functions"
+#md #
+#md #     Flux's `Chains` can also contain functions, e.g. `flatten`.
+#md #     This kind of layer can be registered as
+#md #     ```julia
+#md #     LRP_CONFIG.supports_layer(::typeof(mylayer)) = true
+#md #     ```
+
+# ### Registering activation functions
+# The mechanism for registering custom activation functions is analogous to that of custom layers:
+myrelu(x) = max.(0, x)
+model = Chain(Flux.flatten, Dense(784, 100, myrelu), Dense(100, 10))
+
+# Once again, creating an LRP analyzer for this model will throw an `ArgumentError`
+# and display the following model check summary:
+# ```julia-repl
+# julia> analyzer = LRP(model3)
+# ┌───┬─────────────────────────┬─────────────────┬────────────┬────────────────┐
+# │   │ Layer                   │ Layer supported │ Activation │ Act. supported │
+# ├───┼─────────────────────────┼─────────────────┼────────────┼────────────────┤
+# │ 1 │ flatten                 │            true │     —      │           true │
+# │ 2 │ Dense(784, 100, myrelu) │            true │   myrelu   │          false │
+# │ 3 │ Dense(100, 10)          │            true │  identity  │           true │
+# └───┴─────────────────────────┴─────────────────┴────────────┴────────────────┘
+#   Activations failed model check
+#   ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+#
+#   Found layers with unknown or unsupported activation functions myrelu. LRP assumes that the model is a "deep rectifier network" that only contains ReLU-like activation functions.
+#
+#   If you think the missing activation function should be supported by default, please submit an issue (https://github.com/adrhill/ExplainableAI.jl/issues).
+#
+#   These model checks can be skipped at your own risk by setting the LRP-analyzer keyword argument skip_checks=true.
+#
+#   [...]
+# ```
+
+# Registation works by defining the function [`LRP_CONFIG.supports_activation`](@ref) as `true`:
+LRP_CONFIG.supports_activation(::typeof(myrelu)) = true
+
+# now the analyzer can be created without error:
+analyzer = LRP(model)
+
+# ## How it works internally
+# Internally, ExplainableAI pre-allocates modified layers by dispatching `modify_layer`
+# on rule and layer types. This constructs the `state` of a LRP analyzer.
+#
+# Calling `analyze` on a LRP-model then applies a forward-pass of the model,
+# keeping track of the activations `aₖ` for each layer `k`.
+# The relevance `Rₖ₊₁` is then set to the output neuron activation and the rules are applied
+# in a backward-pass over the model layers and previous activations.
+#
+# This is done by calling low level functions
+# ```julia
+# lrp!(Rₖ, rule, modified_layer, aₖ, Rₖ₊₁)
+#     Rₖ .= ...
+# end
+# ```
+# These functions in-place modify a pre-allocated array of the input relevance `Rₖ`
+# (the `!` is a [naming convention](https://docs.julialang.org/en/v1/manual/style-guide/#bang-convention)
+# in Julia to denote functions that modify their arguments).
+#
+# The correct rule is applied via [multiple dispatch](https://www.youtube.com/watch?v=kc9HwsxE1OY)
+# on the types of the arguments `rule` and `modified_layer`.
+# The relevance `Rₖ` is then computed based on the input activation `aₖ`
+# and the output relevance `Rₖ₊₁`.
+# Multiple dispatch is also used to dispatch `modify_parameters` and `modify_denominator`
+# on the rule and layer type.
+
+# ### Generic rule implementation using automatic differentiation
+# The generic LRP rule–of which the ``0``-, ``\epsilon``- and ``\gamma``-rules are special cases–reads[^1][^2]:
+# ```math
+# R_{j}=\sum_{k} \frac{a_{j} \cdot \rho\left(w_{j k}\right)}{\epsilon+\sum_{0, j} a_{j} \cdot \rho\left(w_{j k}\right)} R_{k}
+# ```
+#
+# where ``\rho`` is a function that modifies parameters – what we call `modify_parameters`.
+#
+# The computation of this propagation rule can be decomposed into four steps:
+# ```math
+# \begin{array}{lr}
+# \forall_{k}: z_{k}=\epsilon+\sum_{0, j} a_{j} \cdot \rho\left(w_{j k}\right) & \text { (forward pass) } \\
+# \forall_{k}: s_{k}=R_{k} / z_{k} & \text { (element-wise division) } \\
+# \forall_{j}: c_{j}=\sum_{k} \rho\left(w_{j k}\right) \cdot s_{k} & \text { (backward pass) } \\
+# \forall_{j}: R_{j}=a_{j} c_{j} & \text { (element-wise product) }
+# \end{array}
+# ```
+#
+# For deep rectifier networks,
+# the third step can be implemented via automatic differentiation (AD).
+#
+# This equation is implemented in ExplainableAI as the default method
+# for all layer types that don't have a specialized implementation.
+# We will refer to it as the "AD fallback".
+#
+# [^1]: G. Montavon et al., [Layer-Wise Relevance Propagation: An Overview](https://link.springer.com/chapter/10.1007/978-3-030-28954-6_10)
+# [^2]: W. Samek et al., [Explaining Deep Neural Networks and Beyond: A Review of Methods and Applications](https://ieeexplore.ieee.org/document/9369420)
+
+# ### AD fallback
+# The default LRP fallback for unknown layers uses AD via [Zygote](https://github.com/FluxML/Zygote.jl).
+# For `lrp!`, we implement the previous four step computation using `Zygote.pullback` to
+# compute ``c`` from the previous equation as a VJP, pulling back ``s_{k}=R_{k}/z_{k}``:
+# ```julia
+# function lrp!(Rₖ, rule, modified_layer, aₖ, Rₖ₊₁)
+#    ãₖ = modify_input(rule, aₖ)
+#    z, back = Zygote.pullback(modified_layer, ãₖ)
+#    s = Rₖ₊₁ ./ modify_denominator(rule, z)
+#    Rₖ .= ãₖ .* only(back(s))
+# end
+# ```
+#
+# You can see how `modify_input` and `modify_denominator` dispatch on rule and layer types.
+# Unknown layers that are registered in the `LRP_CONFIG` use this exact function.
+
+# ### Specialized implementations
+# We can also implement specialized versions of `lrp!` based on the type of `layer`,
+# e.g. reshaping layers.
+#
+# Reshaping layers don't affect attributions. We can therefore avoid the computational
+# overhead of AD by writing a specialized implementation that simply reshapes back:
+# ```julia
+# function lrp!(Rₖ, rule, ::ReshapingLayer, aₖ, Rₖ₊₁)
+#     Rₖ .= reshape(Rₖ₊₁, size(aₖ))
+# end
+# ```
+#
+# Since the rule type didn't matter in this case, we didn't specify it.
+#
+# We can even implement the generic rule as a specialized implementation for `Dense` layers:
+# ```julia
+# function lrp!(Rₖ, rule, layer::Dense, aₖ, Rₖ₊₁)
+#    ãₖ = modify_input(rule, aₖ)
+#    z = modify_denominator(rule, modified_layer(ãₖ))
+#    @tullio Rₖ[j, b] = modified_layer.weight[i, j] * ãₖ[j, b] / z[i, b] * Rₖ₊₁[i, b]
+# end
+# ```
+#
+# For maximum low-level control beyond `modify_input` and `modify_denominator,
+# you can also implement your own `lrp!` function and dispatch
+# on individual rule types `MyRule` and layer types `MyLayer`:
+# ```julia
+# function lrp!(Rₖ, rule::MyRule, layer::MyLayer, aₖ, Rₖ₊₁)
+#     Rₖ .= ...
+# end
+# ```
---
+++ dev/literate/example.jl
@@ -0,0 +1,148 @@
+# # Getting started
+# ExplainableAI.jl can be used on any classifier.
+# In this first example, we will look at attributions on a LeNet5 model that was pretrained on MNIST.
+#
+# ### Loading the model
+#md # !!! note
+#md #
+#md #     Outside of these docs, you should be able to load the model using
+#md #     ```julia
+#md #     using BSON: @load
+#md #     @load "model.bson" model
+#md #     ```
+
+using ExplainableAI
+using Flux
+using BSON
+
+model = BSON.load("../model.bson", @__MODULE__)[:model]
+
+#md # !!! warning "Strip softmax"
+#md #
+#md #     For models with softmax activations on the output, it is necessary to call
+#md #     [`strip_softmax`](@ref)
+#md #     ```julia
+#md #     model = strip_softmax(model)
+#md #     ```
+#md #     before analyzing.
+
+# ### Loading MNIST
+# We use MLDatasets to load a single image from the MNIST dataset:
+using MLDatasets
+using ImageCore
+using ImageIO
+using ImageShow
+
+index = 10
+x, _ = MNIST(Float32, :test)[10]
+
+convert2image(MNIST, x)
+
+# By convention in Flux.jl, this input needs to be resized to WHCN format by adding a color channel and batch dimensions.
+input = reshape(x, 28, 28, 1, :);
+
+#md # !!! warning "Input format"
+#md #
+#md #     For any attribution of a model, ExplainableAI.jl assumes the batch dimension to be come last in the input.
+#md #
+#md #     For the purpose of heatmapping, the input is assumed to be in WHCN order
+#md #     (width, height, channels, batch), which is Flux.jl's convention.
+
+# ## Calling the analyzer
+# We can now select an analyzer of our choice
+# and call [`analyze`](@ref) to get an [`Explanation`](@ref):
+analyzer = LRP(model)
+expl = analyze(input, analyzer);
+
+# This [`Explanation`](@ref) bundles the following data:
+# * `expl.attribution`: the analyzer's attribution
+# * `expl.output`: the model output
+# * `expl.neuron_selection`: the neuron index used for the attribution
+# * `expl.analyzer`: a symbol corresponding the used analyzer, e.g. `:LRP`
+
+# Finally, we can visualize the `Explanation` through [`heatmap`](@ref):
+heatmap(expl)
+
+# Or get the same result by combining both analysis and heatmapping into one step:
+heatmap(input, analyzer)
+
+# ## Neuron selection
+# By passing an additional index to our call to [`analyze`](@ref), we can compute the attribution
+# with respect to a specific output neuron.
+# Let's see why the output wasn't interpreted as a 4 (output neuron at index 5)
+heatmap(input, analyzer, 5)
+
+# This heatmap shows us that the "upper loop" of the hand-drawn 9 has negative relevance
+# with respect to the output neuron corresponding to digit 4!
+
+#md # !!! note
+#md #
+#md #     The ouput neuron can also be specified when calling [`analyze`](@ref):
+#md #     ```julia
+#md #     expl = analyze(img, analyzer, 5)
+#md #     ```
+
+# ## Input batches
+# ExplainableAI also supports input batches:
+batchsize = 100
+xs, _ = MNIST(Float32, :test)[1:batchsize]
+batch = reshape(xs, 28, 28, 1, :) # reshape to WHCN format
+expl_batch = analyze(batch, analyzer);
+
+# This will once again return a single `Explanation` `expl_batch` for the entire batch.
+# Calling `heatmap` on `expl_batch` will detect the batch dimension and return a vector of heatmaps.
+#
+# Let's check if the digit at `index = 10` still matches.
+hs = heatmap(expl_batch)
+hs[index]
+
+# JuliaImages' `mosaic` can be used to return a tiled view of all heatmaps:
+mosaic(hs; nrow=10)
+
+# We can also evaluate a batch with respect to a specific output neuron, e.g. for the digit zero at index `1`:
+mosaic(heatmap(batch, analyzer, 1); nrow=10)
+
+# ## Automatic heatmap presets
+# Currently, the following analyzers are implemented:
+#
+# ```
+# ├── Gradient
+# ├── InputTimesGradient
+# ├── SmoothGrad
+# ├── IntegratedGradients
+# └── LRP
+#     ├── ZeroRule
+#     ├── EpsilonRule
+#     ├── GammaRule
+#     ├── WSquareRule
+#     ├── FlatRule
+#     ├── ZBoxRule
+#     ├── ZPlusRule
+#     ├── AlphaBetaRule
+#     └── PassRule
+# ```
+#
+# Let's try [`InputTimesGradient`](@ref)
+analyzer = InputTimesGradient(model)
+heatmap(input, analyzer)
+
+# and [`Gradient`](@ref)
+analyzer = Gradient(model)
+heatmap(input, analyzer)
+
+# As you can see, the function [`heatmap`](@ref) automatically applies common presets for each method.
+#
+# Since [`InputTimesGradient`](@ref) and [`LRP`](@ref) both compute attributions, their presets are similar.
+# Gradient methods however are typically shown in grayscale.
+
+# ## Custom heatmap settings
+# We can partially or fully override presets by passing keyword arguments to [`heatmap`](@ref):
+using ColorSchemes
+heatmap(expl; cs=ColorSchemes.jet)
+#
+heatmap(expl; reduce=:sum, rangescale=:extrema, cs=ColorSchemes.inferno)
+
+# This also works with batches
+mosaic(heatmap(expl_batch; rangescale=:extrema, cs=ColorSchemes.inferno); nrow=10)
+
+# For the full list of keyword arguments, refer to the [`heatmap`](@ref) documentation.
