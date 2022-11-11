ed9436fe009c983209eb5ecae8b6cd7852b1cff2
Adrian Hill <adrian.hill@mailbox.org>
2022-11-11 15:23:08 +0100

    Restructure docs folder

--- docs/make.jl
+++ docs/make.jl
@@ -2,16 +2,16 @@ using ExplainableAI
 using Documenter
 using Literate
 
-EXAMPLE_DIR = joinpath(@__DIR__, "literate")
+LITERATE_DIR = joinpath(@__DIR__, "src/literate")
 OUT_DIR = joinpath(@__DIR__, "src/generated")
 
 # Use Literate.jl to generate docs and notebooks of examples
-for example in readdir(EXAMPLE_DIR)
-    EXAMPLE = joinpath(EXAMPLE_DIR, example)
+for file in readdir(LITERATE_DIR)
+    path = joinpath(LITERATE_DIR, file)
 
-    Literate.markdown(EXAMPLE, OUT_DIR; documenter=true) # markdown for Documenter.jl
-    Literate.notebook(EXAMPLE, OUT_DIR) # .ipynb notebook
-    Literate.script(EXAMPLE, OUT_DIR) # .jl script
+    Literate.markdown(path, OUT_DIR; documenter=true) # markdown for Documenter.jl
+    Literate.notebook(path, OUT_DIR) # .ipynb notebook
+    Literate.script(path, OUT_DIR) # .jl script
 end
 
 makedocs(;
