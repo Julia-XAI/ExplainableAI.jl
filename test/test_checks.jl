d37609d9c9782cef17faf1e9b0bd6443f7c63d90
Adrian Hill <adrian.hill@mailbox.org>
2022-11-24 20:21:00 +0100

    Formatting fixes for tests

--- test/test_checks.jl
+++ test/test_checks.jl
@@ -4,13 +4,15 @@ using Suppressor
 # Flux layers
 unknown_function(x) = x
 @test check_model(:LRP, Chain(Dense(2, 2, relu)))
-@test_throws ErrorException("Unknown layers or unsupported activation functions found in model") check_model(
-    :LRP, Chain(Dense(2, 2, softmax)); verbose=false
-)
-@test_throws ErrorException("Unknown layers or unsupported activation functions found in model") check_model(
-    :LRP, Chain(unknown_function); verbose=false
-)
-@test_throws ErrorException("Unknown layers or unsupported activation functions found in model") @suppress check_model(
+@test_throws ErrorException(
+    "Unknown layers or unsupported activation functions found in model"
+) check_model(:LRP, Chain(Dense(2, 2, softmax)); verbose=false)
+@test_throws ErrorException(
+    "Unknown layers or unsupported activation functions found in model"
+) check_model(:LRP, Chain(unknown_function); verbose=false)
+@test_throws ErrorException(
+    "Unknown layers or unsupported activation functions found in model"
+) @suppress check_model(
     :LRP,
     Chain(
         unknown_function,
@@ -26,26 +28,26 @@ struct MyLayer{T}
     x::T
 end
 TestLayer = MyLayer(Dense(2, 2, relu))
-@test_throws ErrorException("Unknown layers or unsupported activation functions found in model") check_model(
-    :LRP, Chain(TestLayer); verbose=false
-)
-@test_throws ErrorException("Unknown layers or unsupported activation functions found in model") LRP(
-    Chain(TestLayer); verbose=false
-)
+@test_throws ErrorException(
+    "Unknown layers or unsupported activation functions found in model"
+) check_model(:LRP, Chain(TestLayer); verbose=false)
+@test_throws ErrorException(
+    "Unknown layers or unsupported activation functions found in model"
+) LRP(Chain(TestLayer); verbose=false)
 @test_nowarn LRP(Chain(TestLayer); skip_checks=true)
 ## Test should pass after registering the layer
 LRP_CONFIG.supports_layer(::MyLayer) = true
 @test check_model(:LRP, Chain(TestLayer); verbose=false) == true
 @test_nowarn LRP(Chain(TestLayer))
 ## ...repeat for layers that are functions
-@test_throws ErrorException("Unknown layers or unsupported activation functions found in model") check_model(
-    :LRP, Chain(unknown_function); verbose=false
-)
+@test_throws ErrorException(
+    "Unknown layers or unsupported activation functions found in model"
+) check_model(:LRP, Chain(unknown_function); verbose=false)
 LRP_CONFIG.supports_layer(::typeof(unknown_function)) = true
 @test check_model(:LRP, Chain(unknown_function); verbose=false) == true
 ## ...repeat for activation functions
-@test_throws ErrorException("Unknown layers or unsupported activation functions found in model") check_model(
-    :LRP, Chain(Dense(2, 2, unknown_function)); verbose=false
-)
+@test_throws ErrorException(
+    "Unknown layers or unsupported activation functions found in model"
+) check_model(:LRP, Chain(Dense(2, 2, unknown_function)); verbose=false)
 LRP_CONFIG.supports_activation(::typeof(unknown_function)) = true
 @test check_model(:LRP, Chain(Dense(2, 2, unknown_function)); verbose=false) == true
