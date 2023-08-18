9e8fc70ef30015084d2b859321d40a1b8e37a66c
Adrian Hill <adrian.hill@mailbox.org>
2023-08-18 15:30:05 +0200

    Test LRP on chain of chains

--- test/test_batches.jl
+++ test/test_batches.jl
@@ -21,6 +21,7 @@ input_batch = cat(input1_bd, input2_bd; dims=2)
 
 ANALYZERS = Dict(
     "LRPZero"             => LRP,
+    "LRPZero_COC"         => m -> LRP(m; flatten=false),  # chain of chains
     "Gradient"            => Gradient,
     "InputTimesGradient"  => InputTimesGradient,
     "SmoothGrad"          => m -> SmoothGrad(m, 5, 0.1, MersenneTwister(123)),
