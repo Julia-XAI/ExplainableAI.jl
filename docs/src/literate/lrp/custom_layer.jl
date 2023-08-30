a112e16fb5c46a3403449d172e3399f37e2deabf
Adrian Hill <adrian.hill@mailbox.org>
2023-08-30 19:43:25 +0200

    Update custom layer docs

--- docs/src/literate/lrp/custom_layer.jl
+++ docs/src/literate/lrp/custom_layer.jl
@@ -1,20 +1,26 @@
-# ## Custom layers and activation functions
+# # Supporting new layers and activation functions
+# One of the design goals of ExplainableAI.jl is to combine ease of use and
+# extensibility for the purpose of research.
+# This example will show you how to extent LRP to new layer types and activation functions.
 using Flux
 using ExplainableAI
 
-# ### Model checks for humans
-# Good model checks and presets should allow novice users to apply XAI methods
-# in a "plug & play" manner according to best practices.
+# ## Model checks
+# To assure that novice users use LRP according to best practices,
+# ExplainableAI.jl runs strict model checks when creating an `LRP` analyzer.
 #
-# Let's say we define a layer that doubles its input:
+# Let's demonstrate this by defining a new layer type that doubles its input
 struct MyDoublingLayer end
 (::MyDoublingLayer)(x) = 2 * x
 
 mylayer = MyDoublingLayer()
 mylayer([1, 2, 3])
 
-# Let's append this layer to our model:
-model = Chain(Dense(100, 20), MyDoublingLayer())
+# and by defining a model that uses this layer:
+model = Chain(
+    Dense(100, 20),
+    MyDoublingLayer()
+);
 
 # Creating an LRP analyzer, e.g. `LRP(model)`, will throw an `ArgumentError`
 # and print a summary of the model check in the REPL:
@@ -41,20 +47,15 @@ model = Chain(Dense(100, 20), MyDoublingLayer())
 # ```
 
 # LRP should only be used on deep rectifier networks and ExplainableAI doesn't
-# recognize `MyDoublingLayer` as a compatible layer.
-# By default, it will therefore return an error and a model check summary
+# recognize `MyDoublingLayer` as a compatible layer by default.
+# It will therefore return an error and a model check summary
 # instead of returning an incorrect explanation.
 #
 # However, if we know `MyDoublingLayer` is compatible with deep rectifier networks,
 # we can register it to tell ExplainableAI that it is ok to use.
 # This will be shown in the following section.
 
-#md # !!! warning "Skipping model checks"
-#md #
-#md #     All model checks can be skipped at the user's own risk by setting the LRP-analyzer
-#md #     keyword argument `skip_checks=true`.
-
-# ### Registering custom layers
+# ## Registering layers
 # The error in the model check will stop after registering our custom layer type
 # `MyDoublingLayer` as "supported" by ExplainableAI.
 #
@@ -70,13 +71,17 @@ analyzer = LRP(model)
 #md #     Flux's `Chains` can also contain functions, e.g. `flatten`.
 #md #     This kind of layer can be registered as
 #md #     ```julia
-#md #     LRP_CONFIG.supports_layer(::typeof(mylayer)) = true
+#md #     LRP_CONFIG.supports_layer(::typeof(flatten)) = true
 #md #     ```
 
-# ### Registering activation functions
+# ## Registering activation functions
 # The mechanism for registering custom activation functions is analogous to that of custom layers:
 myrelu(x) = max.(0, x)
-model = Chain(Dense(784, 100, myrelu), Dense(100, 10))
+
+model = Chain(
+    Dense(784, 100, myrelu),
+    Dense(100, 10),
+);
 
 # Once again, creating an LRP analyzer for this model will throw an `ArgumentError`
 # and display the following model check summary:
@@ -98,6 +103,8 @@ model = Chain(Dense(784, 100, myrelu), Dense(100, 10))
 #   If you think the missing layer should be supported by default, please submit an issue (https://github.com/adrhill/ExplainableAI.jl/issues).
 #
 #   [...]
+#
+# ERROR: Unknown layer or activation function found in model
 # ```
 
 # Registation works by defining the function [`LRP_CONFIG.supports_activation`](@ref) as `true`:
@@ -105,3 +112,19 @@ LRP_CONFIG.supports_activation(::typeof(myrelu)) = true
 
 # now the analyzer can be created without error:
 analyzer = LRP(model)
+
+# ## Skipping model checks
+# All model checks can be skipped at the user's own risk by setting the LRP-analyzer
+# keyword argument `skip_checks=true`.
+struct UnknownLayer end
+(::UnknownLayer)(x) = x
+
+unknown_activation(x) = max.(0, x)
+
+model = Chain(Dense(100, 20, unknown_activation), MyDoublingLayer())
+
+LRP(model; skip_checks=true)
+
+# Instead of throwing the usual `# ERROR: Unknown layer or activation function found in model`,
+# the LRP analyzer was created without having to register either the layer `UnknownLayer`
+# or the activation function `unknown_activation`.
