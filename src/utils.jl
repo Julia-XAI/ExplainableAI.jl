f8233c1753f1a9947c6f646c923a1dbd860e35f2
Adrian Hill <adrian.hill@mailbox.org>
2023-12-06 20:30:39 +0100

    Remove `batch_dim_view`, moved into XAIBase

--- src/utils.jl
+++ src/utils.jl
@@ -20,28 +20,6 @@ function safedivide(a::AbstractArray{T}, b::AbstractArray{T}, eps=T(1.0f-9)) whe
 end
 
 """
-    batch_dim_view(A)
-
-Return a view onto the array `A` that contains an extra singleton batch dimension at the end.
-This avoids allocating a new array.
-
-## Example
-```juliarepl
-julia> A = [1 2; 3 4]
-2×2 Matrix{Int64}:
- 1  2
- 3  4
-
-julia> batch_dim_view(A)
-2×2×1 view(::Array{Int64, 3}, 1:2, 1:2, :) with eltype Int64:
-[:, :, 1] =
- 1  2
- 3  4
-```
-"""
-batch_dim_view(A::AbstractArray{T,N}) where {T,N} = view(A, ntuple(Returns(:), N + 1)...)
-
-"""
     drop_batch_index(I)
 
 Drop batch dimension index (last value) from CartesianIndex.
