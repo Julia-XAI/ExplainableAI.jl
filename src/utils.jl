930038c8923db7c5132b598dcaf3105e6eac5657
Adrian Hill <adrian.hill@mailbox.org>
2024-01-30 18:44:08 +0100

    Improve code coverage

--- src/utils.jl
+++ src/utils.jl
@@ -10,29 +10,6 @@ CartesianIndex(5, 3)
 drop_batch_index(C::CartesianIndex) = CartesianIndex(C.I[1:(end - 1)])
 
 """
-    ones_like(x)
-
-Returns array of ones of same shape and type as `x`.
-
-## Example
-```julia-repl
-julia> x = rand(Float16, 2, 4, 1)
-2×4×1 Array{Float16, 3}:
-[:, :, 1] =
- 0.2148  0.9053   0.751    0.358
- 0.38    0.09033  0.04053  0.6543
-
-julia> ones_like(x)
-2×4×1 Array{Float16, 3}:
-[:, :, 1] =
- 1.0  1.0  1.0  1.0
- 1.0  1.0  1.0  1.0
-```
-"""
-ones_like(x::AbstractArray) = fill!(similar(x), 1)
-ones_like(x::Number) = oneunit(x)
-
-"""
     masked_copy(A, mask)
 
 Return a copy of A on which `mask` was applied.
